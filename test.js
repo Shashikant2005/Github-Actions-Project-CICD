5e1048348f9d020ff4cf5d8dde0c630da42d1f1e
shashikant jadhav <jadhavshashikant677@gmail.com>
2025-10-08 00:44:49 +0530

    third commit

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const greet = require("./index");
 function testGreet() {
   const output = greet("World");
   if (output === "Hello, World!") {
-    console.log("✅ Test passed");
+    console.log("✅ Test cases passed");
     process.exit(0);
   } else {
     console.error("❌ Test cases failed");
